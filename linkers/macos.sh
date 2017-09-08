f04515527c40e665cc5f59b6418d71f7733899a4
Stefan Countryman <stefan.countryman@gmail.com>
2017-09-08 14:59:08 -0400

    organize linked/copied files into directories

--- linkers/macos.sh
+++ linkers/macos.sh
@@ -3,25 +3,30 @@
 # link and copy things from the dotfiles folder.
 # assumes dotfiles are in ~/dev/dotfiles.
 
-cd ~
+# if "-f" flag is provided, overwrite everything
+if [ "$1"z = -fz ]; then
+    F=f
+else
+    F=""
+fi
 
 # these files should be symlinked
-for rcfile in .bashrc .vimrc .tmux.conf .muttrc .mailrc .gitignore_global .inputrc; do
-    if [ -e "${rcfile}" ]; then
-        echo "${rcfile} exists, skipping (delete and rerun to link it)"
+for rcfile in $(find linkfiles -iname '.*' -exec basename {} \; ); do
+    if [ -L ~/"${rcfile}" ] || [ -e ~/"${rcfile}" ] && ! [ "$1"z = -fz ]; then
+        echo "${rcfile} exists, skipping (delete to link it or use -f flag)"
     else
         echo "linking ${rcfile}..."
-        ln -s {dev/dotfiles/,}"${rcfile}"
+	ln -${F}s {dev/dotfiles/linkfiles,~}/"${rcfile}"
     fi
 done
 
 # for now, these files should be copied, since they will be populated with
 # sensitive information (TODO: fix this with auto-redact git hooks)
-for rcfile in .pypirc .gitconfig .msmtprc .offlineimaprc; do
-    if [ -e "${rcfile}" ]; then
-        echo "${rcfile} exists, skipping (delete and rerun to recopy it)"
+for rcfile in $(find ~/dev/dotfiles/copyfiles -iname '.*' -exec basename {} \; ); do
+    if [ -L ~/"${rcfile}" ] || [ -e ~/"${rcfile}" ] && ! [ "$1"z = -fz ]; then
+        echo "${rcfile} exists, skipping (delete to copy it or use -f flag)"
     else
         echo "copying ${rcfile}..."
-        cp {dev/dotfiles/,}"${rcfile}"
+	cp -${F}R ~/{dev/dotfiles/copyfiles/,}"${rcfile}"
     fi
 done
