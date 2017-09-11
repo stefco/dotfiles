0979d58fad278ca37d2ba76f1d32af6d894ef7af
Stefan Countryman <stefan.countryman@gmail.com>
2017-09-11 03:52:53 -0400

    fix gwhen path typo

--- linkfiles/.ipython/profile_gwhen/startup/40-gwhen.py
+++ linkfiles/.ipython/profile_gwhen/startup/40-gwhen.py
@@ -8,12 +8,13 @@ import scipy.io.matlab
 GWHEN_DIRS = ['~/multimessenger-pipeline', '~/dev/multimessenger-pipeline']
 for gwhendir in [os.path.expanduser(d) for d in GWHEN_DIRS]:
     if os.path.exists(gwhendir):
+        print('GWHEN dir found: {}'.format(gwhendir))
         sys.path.append(gwhendir)
         sys.path.append(os.path.join(gwhendir, 'gwhen', 'bin'))
         sys.path.append(os.path.join(gwhendir, 'gwhen', 'file_handlers'))
-    import gwhen
-    from gwhen.file_handlers.icecube_utils import realtime_tools
-    from IceCubeNeutrinoList_txt import zen_az2ra_dec, ra_dec2zen_az
+import gwhen
+from gwhen.file_handlers.icecube_utils import realtime_tools
+from IceCubeNeutrinoList_txt import zen_az2ra_dec, ra_dec2zen_az
 
 # initialize an event here with variable name "e" for quick work.
 print('Setting `e` to an event in the current working directory...')
