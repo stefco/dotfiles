bb22c2f673e97ff1f11546d63e990bede4bb2526
Stefan Countryman <stefan.countryman@gmail.com>
2017-09-13 14:40:44 -0400

    Add gcn_notice_filename function to igrace

--- linkfiles/.config/ipython/profile_grace/startup/30-grace.py
+++ linkfiles/.config/ipython/profile_grace/startup/30-grace.py
@@ -2,3 +2,11 @@
 # set up an interactive environment with gracedb rest api access.
 import ligo.gracedb.rest
 client = ligo.gracedb.rest.GraceDb()
+
+def gcn_notice_filenames(graceids):
+    """Take a list of GraceIDs and check whether they have LVC GCN-notices. If
+    so, print those notice filenames for GraceDB."""
+    for gid in graceids:
+        print("GraceID: {}".format(gid))
+        f = client.files(gid).json()
+        print filter(lambda k: 'Initial' in k, f.keys())
