46f3322bc512b1d4fe78eda00b88f1bed72513f5
Stefan Countryman <stefan.countryman@gmail.com>
2017-12-11 10:50:43 -0500

    Add ffmpeg-python to pip installation

--- provisioners/macos.sh
+++ provisioners/macos.sh
@@ -86,7 +86,7 @@ echo "  https://gwpy.github.io/docs/stable/install/index.html" | tee -a $log
 pip install gwpy[all] >$log 2>$errlog
 
 echo "Installing pip packages." | tee -a $log
-pip install PyForms itermplot
+pip install PyForms itermplot ffmpeg-python
 
 logdate
 tee -a <<"__EOF__"
