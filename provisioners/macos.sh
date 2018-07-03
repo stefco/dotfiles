ac3b6ccdf7f3217c3d45fc576e6a32b1fc165982
Stefan Countryman <stefan.countryman@gmail.com>
2018-07-03 14:16:11 -0400

    add npm6 to provisioners

--- provisioners/macos.sh
+++ provisioners/macos.sh
@@ -34,7 +34,7 @@ port -f install >$log 2>$errlog \
     julia libcaca msmtp offlineimap vim ncdu neomutt notmuch OpenBLAS pstree \
     psutils tree readline dtrx fortune bash-completion poppler djvulibre \
     unrar tiff jp2a shellcheck p5.24-term-readline-gnu \
-    findutils youtube-dl qrencode ripgrep optipng
+    findutils youtube-dl qrencode ripgrep optipng npm6
 # add the MacPorts bash binary to the list of shells
 bash -c "echo /opt/local/bin/bash >>/etc/shells"
 
