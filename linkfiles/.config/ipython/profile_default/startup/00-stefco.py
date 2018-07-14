60b267b8e9eecfd88bf6642d4eb0a544c6d80e9e
Stefan Countryman <stefan.countryman@gmail.com>
2018-07-14 18:12:02 -0400

    add datetime and glob to ipython startup

--- linkfiles/.config/ipython/profile_default/startup/00-stefco.py
+++ linkfiles/.config/ipython/profile_default/startup/00-stefco.py
@@ -3,6 +3,8 @@
 import sys
 import os
 import psutil
+import datetime
+from glob import glob
 import IPython.core.magic
 import IPython.utils.path
 sys.path.append(os.path.expanduser('~/dev/geco_data'))
