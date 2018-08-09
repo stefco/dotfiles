7dd7603c89f78ab529707b72bdb0ac08291ebb1e
Stefan Countryman <stefan.countryman@gmail.com>
2018-08-09 19:09:13 -0400

    Add richer gracedb event timeline parsing

--- linkfiles/.config/ipython/profile_grace/startup/30-grace.py
+++ linkfiles/.config/ipython/profile_grace/startup/30-grace.py
@@ -12,22 +12,53 @@ def gcn_notice_filenames(graceids):
         f = client.files(gid).json()
         print filter(lambda k: 'Initial' in k, f.keys())
 
-def get_event_times(triggers):
+def get_event_times(triggers, subtractgpstime=True):
     """Take a GraceDB event iterator and return a list of GPS times for
-    milestones in each trigger in the ``triggers`` iterable."""
-    return {
-        e['graceid']: {
-            'emready': [
-                utc2gps(dateutil.parser.parse(l['created']))
+    milestones in each trigger in the ``triggers`` iterable (of the sort
+    returned by ``ligo.gracedb.rest.GraceDb().events()``). If
+    ``subtractgpstime`` is ``True``, return seconds past GPS time for each
+    milestone time (except ``gpstime``, which remains the absolute gps time of
+    the reconstructed event)."""
+    times = dict()
+    for e in triggers:
+        gpstime = float(e['gpstime'])
+        if subtractgpstime:
+            t0 = gpstime
+        else:
+            t0 = 0.
+        logs = client.logs(e['graceid']).json()['log']
+        times[e['graceid']] = {
+            'emready': ([
+                utc2gps(dateutil.parser.parse(l['created'])) - t0
                 for l in client.labels(e['graceid']).json()['labels']
                 if l['name'] == 'EM_READY'
-            ][0],
-            'created': utc2gps(dateutil.parser.parse(e['created'])),
-            'gpstime': float(e['gpstime']),
-            'lvcinitial': min([
-                utc2gps(dateutil.parser.parse(log['created']))
-                for log in client.logs(e['graceid']).json()['log']
-                if 'Initial.xml' in log['filename']
-            ]),
-        } for e in triggers
-    }
+            ] or [None])[0],
+            'created': utc2gps(dateutil.parser.parse(e['created'])) - t0,
+            'gpstime': gpstime,
+            'gcn': {
+                'lvcinitial': min([
+                    utc2gps(dateutil.parser.parse(log['created'])) - t0
+                    for log in logs
+                    if 'Initial.xml' in log['filename']
+                ] or None),
+                'gcncirc': min([
+                    utc2gps(dateutil.parser.parse(log['created'])) - t0
+                    for log in logs
+                    if log['filename'].startswith("gcncirc")
+                ] or [None]),
+            },
+            'gwhenuploads': {
+                'neutrinos': min([
+                    utc2gps(dateutil.parser.parse(log['created'])) - t0
+                    for log in logs
+                    if log['filename'] == 'IceCubeNeutrinoList.json'
+                ] or [None]),
+                'skymap': min([
+                    utc2gps(dateutil.parser.parse(log['created'])) - t0
+                    for log in logs
+                    if
+                    log['filename'].startswith('coinc_skymap_initial_icecube')
+                ] or [None]),
+            },
+        }
+    return times
