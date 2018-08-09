25868ed8673b637f6a9f36d3ad0f2db78b9b9901
Stefan Countryman <stefan.countryman@gmail.com>
2018-08-09 17:51:27 -0400

    add command for getting times of gracedb event milestones

--- linkfiles/.config/ipython/profile_grace/startup/30-grace.py
+++ linkfiles/.config/ipython/profile_grace/startup/30-grace.py
@@ -2,6 +2,7 @@
 # set up an interactive environment with gracedb rest api access.
 import ligo.gracedb.rest
 client = ligo.gracedb.rest.GraceDb()
+import dateutil
 
 def gcn_notice_filenames(graceids):
     """Take a list of GraceIDs and check whether they have LVC GCN-notices. If
@@ -10,3 +11,23 @@ def gcn_notice_filenames(graceids):
         print("GraceID: {}".format(gid))
         f = client.files(gid).json()
         print filter(lambda k: 'Initial' in k, f.keys())
+
+def get_event_times(triggers):
+    """Take a GraceDB event iterator and return a list of GPS times for
+    milestones in each trigger in the ``triggers`` iterable."""
+    return {
+        e['graceid']: {
+            'emready': [
+                utc2gps(dateutil.parser.parse(l['created']))
+                for l in client.labels(e['graceid']).json()['labels']
+                if l['name'] == 'EM_READY'
+            ][0],
+            'created': utc2gps(dateutil.parser.parse(e['created'])),
+            'gpstime': float(e['gpstime']),
+            'lvcinitial': min([
+                utc2gps(dateutil.parser.parse(log['created']))
+                for log in client.logs(e['graceid']).json()['log']
+                if 'Initial.xml' in log['filename']
+            ]),
+        } for e in triggers
+    }
