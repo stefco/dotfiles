dfa503c184c95c41754d46acbf67fca456e0b55e
Stefan Countryman <stefan.countryman@gmail.com>
2022-10-16 17:34:29 -0400

    plexadd works on nested directories in a single pass

--- winscripts/plexadd.py
+++ winscripts/plexadd.py
@@ -5,6 +5,16 @@ import re
 from tkinter import messagebox, filedialog, simpledialog, Tk
 
 
+def show_contents(src_files):
+    files = [*src_files]
+    return str(len(files)) + " files found:\n\n" + "\n".join(str(p) for p
+                                                                 in files)
+
+
+def get_rel_to(parent):
+    return lambda p: p.relative_to(parent)
+
+
 # https://runestone.academy/runestone/books/published/thinkcspy/GUIandEventDrivenProgramming/02_standard_dialog_boxes.html
 def add_tv():
     tkr = Tk()
@@ -15,9 +25,18 @@ def add_tv():
         if src_raw == '':
             return
         src = Path(src_raw)
-        ok = messagebox.askyesnocancel("Contents okay?", (
-            "Contents of "+str(src)+":\n\n"+"\n".join(p.name for p in src.iterdir())+
-            "\n\nAccept?"))
+        rel = get_rel_to(src)
+        subglob = simpledialog.askstring(
+            "File Globs",
+            "File glob patterns to search? (comma-separated)",
+            parent=tkr,
+            initialvalue='*'
+        ).split(',')
+        src_files = [f for fs in (src.glob(g) for g in subglob) for f in fs]
+        ok = messagebox.askyesnocancel(
+            "Contents okay?",
+            show_contents(map(rel, src_files))
+        )
         if ok is None:
             return
     root = Path(src.anchor)
@@ -33,48 +52,71 @@ def add_tv():
         out_func = out_func or r'lambda m: f"'+dst.name+' S01E{int(m[1]):02d}.{m[2]}"'
         ok = False
         while not ok:
-            in_pat = simpledialog.askstring("Input file regex", "Regular expression for inputs?", parent=tkr,
-                                            initialvalue=in_pat)
+            in_pat = simpledialog.askstring(
+                "Input file regex",
+                "Regular expression for inputs?",
+                parent=tkr,
+                initialvalue=in_pat
+            )
             reg = re.compile(in_pat)
-            ok = messagebox.askyesnocancel("Accept Input Matches?", "\n".join(str(reg.match(s.name)) for s in src.iterdir()))
+            ok = messagebox.askyesnocancel(
+                "Accept Input Matches?",
+                "\n".join(str(reg.match(str(s))) for s in map(rel, src_files))
+            )
             if ok is None:
                 return
             if not ok:
                 messagebox.showinfo("Contents",
-                    "Contents of "+str(src)+":\n\n"+"\n".join(p.name for p in src.iterdir()))
+                                    show_contents(map(rel, src_files)))
         ok = False
         while not ok:
-            filt_raw = simpledialog.askstring("Filter", "Filter function for files?", parent=tkr,
-                                          initialvalue=filt_raw)
+            filt_raw = simpledialog.askstring(
+                "Filter",
+                "Filter function for files?",
+                parent=tkr,
+                initialvalue=filt_raw
+            )
             try:
                 filt = eval(filt_raw)
+                ok = messagebox.askyesnocancel(
+                    "Filtered files",
+                    (
+                        "Filtered files:\n\n" +
+                        "\n".join(
+                            str(s) for s in map(rel, src_files)
+                            if (lambda m: m and filt(m))(reg.match(s.name))
+                        ) + "\n\nContinue?"
+                    )
+                )
+                if ok is None:
+                    return
             except SyntaxError:
                 if messagebox.askretrycancel("Syntax Error", "Syntax Error. Retry?"):
                     continue
                 else:
                     return
-            ok = messagebox.askyesnocancel("Filtered files", "Filtered files:\n\n" +
-                                           "\n".join(s.name for s in src.iterdir()
-                                                     if (lambda m: m and filt(m))(reg.match(s.name))) +
-                                           "\n\nContinue?")
-            if ok is None:
-                return
         ok = False
         while not ok:
-            out_func = simpledialog.askstring("Output name lambda", "Function literal for output filename?",
-                                              parent=tkr, initialvalue=out_func)
+            out_func = simpledialog.askstring(
+                "Output name lambda",
+                "Function literal for output filename?",
+                parent=tkr,
+                initialvalue=out_func
+            )
             try:
                 func = eval(out_func)
+                links = [(s, dst/reg.sub(func, str(rel(s))))
+                         for s in src_files
+                         if (lambda m: m and filt(m))(reg.match(s.name))]
+                ok = messagebox.askyesnocancel("Accept Links?", "\n".join(f"{s.name} -> {d}" for s, d in links) +
+                                            "\n\nACCEPT AND PROCEED?")
+                if ok is None:
+                    return
             except SyntaxError:
                 if messagebox.askretrycancel("Syntax Error", "Syntax Error. Retry?"):
                     continue
                 else:
                     return
-            links = [(s, dst/reg.sub(func, s.name)) for s in src.iterdir() if (lambda m: m and filt(m))(reg.match(s.name))]
-            ok = messagebox.askyesnocancel("Accept Links?", "\n".join(f"{s.name} -> {d}" for s, d in links) +
-                                           "\n\nACCEPT AND PROCEED?")
-            if ok is None:
-                return
         try:
             dst.mkdir(exist_ok=True)
         except OSError as e:
