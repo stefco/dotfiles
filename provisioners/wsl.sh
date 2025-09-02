1a3165380a3d45025b38c7e2b25425ab17db1d3e
Stefan Countryman <stefan.countryman@gmail.com>
2025-09-02 09:57:13 -0400

    install emacs, C++ deps, WGSL, and cargo

--- provisioners/wsl.sh
+++ provisioners/wsl.sh
@@ -1,6 +1,9 @@
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install \
+    git \
+    git-lfs \
+    curl \
     gcc \
     g++ \
     neovim \
@@ -16,15 +19,33 @@ git clone https://github.com/LazyVim/starter ~/.config/lazyvim --depth 1
 mkdir ~/.tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# install anaconda
+curl -O https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash ~/Anaconda3-2024.10-1-Linux-x86_64.sh -b -p $HOME/anaconda3
+source ~/anaconda3/bin/activate
+conda create -n emacs
+conda activate emacs
+conda install -y python pip
+
 # install dotnet for C# LSP
 sudo apt install -y dotnet-sdk-8.0
 sudo apt install -y mono-devel
+
+# install C++ mode deps
+sudo apt install -y clang clangd clang-format
+sudo apt install -y glslang-dev glslang-tools
 
 # install new emacs + doom
 sudo snap install emacs --classic
 git clone https://github.com/hlissner/doom-emacs ~/.emacs.d
 ~/.emacs.d/bin/doom install
 ~/.emacs.d/bin/doom sync
+
+# install cargo
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# install WGSL (WebGPU Shader Language) LSP support
+cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer wgsl_analyzer
 
 cat <<__EOF__
 YOU'LL STILL HAVE TO RUN THE FOLLOWING TO GET DOOM EMACS FULLY WORKING:
