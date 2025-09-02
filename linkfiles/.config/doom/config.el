66da9f53c98834334f1995407079cf1fbbe13a44
Stefan Countryman <stefan.countryman@gmail.com>
2025-09-02 10:12:59 -0400

    launch debugger on error in emacs

--- linkfiles/.config/doom/config.el
+++ linkfiles/.config/doom/config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; LAUNCH DEBUGGER (stack trace) ON ERROR
+;(setq debug-on-error t)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
